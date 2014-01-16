84c3dd3eeb18b07f6deadb74ec2fca608ff55c46
Wesley <wesley@MacBook-Pro-de-Wesley.local>
2014-01-16 10:35:43 -0200

    Initial Commit

---
+++ Game/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Game
+//
+//  Created by Wesley on 16/01/14.
+//  Copyright (c) 2014 Wesley. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Game/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  Game
+//
+//  Created by Wesley on 16/01/14.
+//  Copyright (c) 2014 Wesley. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
